161f8b4de24d05f3611b5383faeaeca380a4e98b
vedavyas <vyasatadkod90@gmail.com>
2025-12-24 13:10:38 +0530

    updated workflow and build issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
+const path = require('path');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { shareAll } = require('@angular-architects/module-federation/webpack');
+const { output } = require('@angular/core');
+const { library, experiments } = require('webpack');
 
-  name: 'my-products-mfe',
-
-  exposes: {
-    './routes': './src/app/app.routes.ts',
-    './StandaloneComponent': './src/app/standalone-component/standalone-component.ts',
+module.exports = {
+  output: {
+    uniqueName: 'my-products-mfe',
+    publicPath: 'auto',
   },
-
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  optimization: {
+    runtimeChunk: false,
   },
-});
+  experiments: {
+    outputModule: true,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      library: { type: 'module'},
+      name: 'my-products-mfe',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './routes': './src/app/app.routes.ts',
+        './StandaloneComponent': './src/app/standalone-component/standalone-component.ts',
+      },
+      shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+      },
+    })
+  ],
+
+
+};
